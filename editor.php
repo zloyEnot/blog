a2ea9db59ca1b9130e80669da0c3405f0f4b877e
zloyEnot <malahovv1992@gmail.com>
2016-07-06 22:42:19 +0300

    Консоль редактора

---
+++ editor.php
@@ -0,0 +1,1 @@
+<?phpinclude_once('startup.php');include_once('model.php');// Установка параметров, подключение к БД, запуск сессии.startup();// Извлечение статей.$articles = articles_all();// Кодировка.header('Content-type: text/html; charset=utf-8');// Вывод в шаблон.include('theme/editor.php');
