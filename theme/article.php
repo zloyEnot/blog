4f4f9fd00b2aa6f86d6754db200c7c044141b03f
zloyEnot <malahovv1992@gmail.com>
2016-07-06 22:35:09 +0300

    Отображение статей

---
+++ theme/article.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html>
+<head>
+    <title>PHP. Уровень 2</title>
+    <meta content="text/html; charset=utf-8" http-equiv="content-type">
+    <link rel="stylesheet" type="text/css" media="screen" href="theme/style.css"/>
+</head>
+<body>
+<h1>PHP. Уровень 2</h1>
+<br/>
+<a href="index.php">Главная</a> |
+<a href="editor.php">Консоль редактора</a>
+<hr/>
+<h1><?= $title ?></h1>
+
+<p><?= $content ?></p>
+<hr/>
+</body>
+</html>
